c048598fc4668a09e72487a15dd3004546bb833a
Troy Willmot <6725891+Yortw@users.noreply.github.com>
2022-02-25 21:05:26 +1300

    Refactor into single multi-target project. Attempt to enable source link.

--- src/Ladon/AssemblyInfo.cs
+++ src/Ladon/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -6,12 +6,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if NET40
 [assembly: AssemblyTitle("Ladon.Net40")]
+#elif NET45
+[assembly: AssemblyTitle("Ladon.Net45")]
+#elif NET48
+[assembly: AssemblyTitle("Ladon.Net48")]
+#elif NETSTANDARD2_0
+[assembly: AssemblyTitle("Ladon.NetStandard20")]
+#endif
 
+#if !NETCORE45
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c69c7fc-acbf-4c6b-b81b-c65017cc4063")]
+#endif
